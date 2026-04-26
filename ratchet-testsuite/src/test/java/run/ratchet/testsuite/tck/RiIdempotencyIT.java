202f27885d35eb0fcf1a209118990860cb1435f5
Jonathan Putney <jonathan@putney.io>
2026-04-26 13:08:22 -0400

    Add RiIdempotencyIT subclass for ratchet-tck-api
    Wires AbstractIdempotencyContract against the wall-clock RiRatchetTckRuntime.
    The contract has no clock dependency; the production MySQL store enforces
    business-key uniqueness via the bkres reservation table, which is what the
    concurrent-submit test exercises.
    
    Three contract methods now run under -P wildfly-managed,mysql.
    
    Signed-off-by: Jonathan Putney <jonathan@putney.io>

---
+++ ratchet-testsuite/src/test/java/run/ratchet/testsuite/tck/RiIdempotencyIT.java
@@ -0,0 +1,39 @@
+package run.ratchet.testsuite.tck;
+
+import run.ratchet.tck.api.AbstractIdempotencyContract;
+import run.ratchet.tck.api.RatchetTckRuntime;
+import run.ratchet.tck.api.TckJobs;
+import run.ratchet.tck.util.ConcurrentTestRunner;
+import run.ratchet.testsuite.util.RatchetArchiveBuilder;
+import jakarta.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+/** RI subclass of {@link AbstractIdempotencyContract}. */
+@ExtendWith(ArquillianExtension.class)
+class RiIdempotencyIT extends AbstractIdempotencyContract {
+
+  @Inject private RiRatchetTckRuntime runtime;
+
+  @Override
+  protected RatchetTckRuntime runtime() {
+    return runtime;
+  }
+
+  @Deployment
+  public static WebArchive createDeployment() {
+    String dbType = System.getProperty("ratchet.test.db.type", "mysql");
+    String profile = System.getProperty("testsuite.profile", "wildfly-managed");
+
+    return RatchetArchiveBuilder.create()
+        .addRatchetDependencies(profile, dbType)
+        .addPackage(RatchetTckRuntime.class.getPackage())
+        .addPackage(ConcurrentTestRunner.class.getPackage())
+        .addClasses(RiRatchetTckRuntime.class, ListenerProbe.class, TckJobs.class)
+        .addStoreInfrastructure()
+        .addBeansXml()
+        .build();
+  }
+}
