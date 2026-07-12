b3f4208d68e56285af83697933796dfd22ca2a5a
jcputney <jonathan@putney.io>
2026-07-12 17:40:13 -0400

    test(schema): expect the full migration sequence
    Signed-off-by: jcputney <jonathan@putney.io>

--- stores/ratchet-store-mysql/src/test/java/run/ratchet/store/mysql/MysqlSchemaMigrationDialectTest.java
+++ stores/ratchet-store-mysql/src/test/java/run/ratchet/store/mysql/MysqlSchemaMigrationDialectTest.java
@@ -118,7 +118,8 @@ class MysqlSchemaMigrationDialectTest {
             .orElseThrow();
 
     assertEquals(
-        List.of("001", "002"), migrations.stream().map(script -> script.version()).toList());
+        List.of("001", "002", "003", "004", "005"),
+        migrations.stream().map(script -> script.version()).toList());
     assertEquals(
         "0b339e555cddc589c0844184a04e2eff8f803bc7d1ef18a695b02dacb1224112", v001.checksum());
   }
